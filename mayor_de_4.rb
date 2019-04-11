5a028fa07c7af20ffb9dbebb3437def95f35d879
Anibal Linares <anibal.linares.19@gmail.com>
2019-04-10 20:07:09 -0400

    Se agrega comentarios

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -1,14 +1,16 @@
-numero1 = ARGV[0].to_i
-numero2 = ARGV[1].to_i
-numero3 = ARGV[2].to_i
-numero4 = ARGV[3].to_i
+  numero1 = ARGV[0].to_i
+  numero2 = ARGV[1].to_i
+  numero3 = ARGV[2].to_i
+  numero4 = ARGV[3].to_i
 
-if (numero1 >= numero2 and numero1 >= numero3 and numero1 >= numero4)
-    puts "#{numero1} es número el mayor"
-elsif (numero2 >= numero1 and numero2 >= numero3 and numero2 >= numero4)
-    puts "#{numero2} es el número mayor"
-elsif (numero3 >= numero1 and numero3 >= numero2 and numero3 >= numero4)
-    puts "#{numero3} es el número mayor"
-elsif (numero4 >= numero1 and numero4 >= numero2 and numero4 >= numero3)
-    puts "#{numero4}  es el número mayor"
-end
+ # Evaluara que numero ingresado es mayor
+
+  if numero1.to_i >= numero2.to_i and numero1.to_i >= numero3.to_i and numero1.to_i >= numero4.to_i
+      puts "#{numero1} es número el mayor"
+  elsif numero2.to_i >= numero1.to_i and numero2.to_i >= numero3.to_i and numero2.to_i >= numero4.to_i
+      puts "#{numero2} es el número mayor"
+  elsif numero3.to_i >= numero1.to_i and numero3.to_i >= numero2.to_i and numero3.to_i >= numero4.to_i
+      puts "#{numero3} es el número mayor"
+  elsif numero4.to_i >= numero1.to_i and numero4.to_i >= numero2.to_i and numero4.to_i >= numero3.to_i
+      puts "#{numero4}  es el número mayor"
+  end
